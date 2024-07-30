1d47310458eda7898906c5dd99f8670115915be1
Amjedusman <amjadusman98@gmail.com>
2024-07-31 00:07:33 +0530

    dynamic images are added to banner

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,23 +1,40 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react'
-import "./Banner.css"
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react';
+import './Banner.css';
+import axios from '../../axios';
+import { API_KEY } from '../../constants/constant';
 
 function Banner() {
+  const [movie, setMovie] = useState();
+
+  useEffect(() => {
+    axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`)
+      .then((response) => {
+        console.log(response.data.results[0]);
+        setMovie(response.data.results[0]);
+      })
+      .catch((error) => {
+        console.error('Error fetching the movie data:', error);
+      });
+  }, []);
+
+  const backgroundImage = movie ? `url("https://image.tmdb.org/t/p/original${movie.backdrop_path}")` : 'url("https://wallpaperaccess.com/full/2703652.png")';
+
   return (
-    <div className='banner'>
-        <div className="content">
-            <h1 className='title'>Movie Name</h1>
-            <div className="banner_buttons">
-                <button className='button'>Play</button>
-                <button className='button'>Mylist</button>
-            </div>
-            <h1 className="description">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod non placeat perspiciatis sunt obcaecati harum aperiam, magnam voluptate deleniti iste, ipsam facere. Officiis neque similique temporibus debitis sed quisquam ipsum.
-            </h1>
+    <div className='banner' style={{ backgroundImage }}>
+      <div className="content">
+        <h1 className='title'>{movie?.title}</h1>
+        <div className="banner_buttons">
+          <button className='button'>Play</button>
+          <button className='button'>My List</button>
         </div>
-        <div className="fade_bottom"></div>
+        <h1 className="description">
+          {movie?.overview}
+        </h1>
+      </div>
+      <div className="fade_bottom"></div>
     </div>
-  )
+  );
 }
 
-export default Banner
+export default Banner;
---
+++ src/constants/constant.jsx
@@ -0,0 +1,2 @@
+export const baseUrl = "https://api.themoviedb.org/3";
+export const API_KEY="58acd7801f2917955c1636fb68195423";
