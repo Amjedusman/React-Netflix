78bdb3746dad1de62548991a3a7d0d4d04ffde92
Amjedusman <amjadusman98@gmail.com>
2024-07-31 00:33:56 +0530

    refresh shows different banners

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import './Banner.css';
 import axios from '../../axios';
-import { API_KEY } from '../../constants/constant';
+import { API_KEY, imageurl } from '../../constants/constant';
 
 function Banner() {
   const [movie, setMovie] = useState();
@@ -10,20 +10,21 @@ function Banner() {
   useEffect(() => {
     axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`)
       .then((response) => {
-        console.log(response.data.results[0]);
-        setMovie(response.data.results[0]);
+        const randomMovie = response.data.results[Math.floor(Math.random() * response.data.results.length)];
+        console.log(randomMovie);
+        setMovie(randomMovie);
       })
       .catch((error) => {
         console.error('Error fetching the movie data:', error);
       });
   }, []);
 
-  const backgroundImage = movie ? `url("https://image.tmdb.org/t/p/original${movie.backdrop_path}")` : 'url("https://wallpaperaccess.com/full/2703652.png")';
+  const backgroundImage = movie ? `url("${imageurl}${movie.backdrop_path}")` : 'url("https://wallpaperaccess.com/full/2703652.png")';
 
   return (
     <div className='banner' style={{ backgroundImage }}>
       <div className="content">
-        <h1 className='title'>{movie?.title}</h1>
+        <h1 className='title'>{movie?.title || movie?.name || movie?.original_name}</h1>
         <div className="banner_buttons">
           <button className='button'>Play</button>
           <button className='button'>My List</button>
--- src/constants/constant.jsx
+++ src/constants/constant.jsx
@@ -1,2 +1,3 @@
 export const baseUrl = "https://api.themoviedb.org/3";
 export const API_KEY="58acd7801f2917955c1636fb68195423";
+export const imageurl="https://image.tmdb.org/t/p/original";
